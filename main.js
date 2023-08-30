06d82742197120adea72c00133932b4bb47318f9
陈浩 <chenhao2110@qq.com>
2023-08-30 12:25:09 +0800

    创建rest函数

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const btn = document.querySelector('button')
 const icecream = document.querySelector('.icecream')
 
+function reset() {
+  icecream.innerHTML = ''
+}
+
 btn.addEventListener('click', () => {
   icecream.insertAdjacentHTML('afterend', '🥗')
 })
